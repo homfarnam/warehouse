d5ddbcb885743aed41b879c27bb827979b91b3c4
homfarnam <homfarnam@gmail.com>
2022-10-05 00:12:44 +0200

    Add hooks folder
    - Create useProducts to get products data
    - Create useArticle to get one article data

---
+++ src/hooks/useProducts.tsx
@@ -0,0 +1,64 @@
+import axios, { AxiosError } from "axios"
+import { useEffect, useReducer } from "react"
+import { toast } from "react-toastify"
+import { ProductApi, warehouseAPI } from "../api"
+import { productsReducer } from "../reducers"
+import { productsInitialState } from "../reducers/productsReducer"
+import { ProductsType } from "../types/api.types"
+
+const useProducts = () => {
+  const [
+    { products, loading: productsLoading, error: productsError },
+    productDispatch,
+  ] = useReducer(productsReducer, productsInitialState)
+
+  const refetchRequest = async (err: AxiosError) => {
+    warehouseAPI(err.config)
+  }
+
+  const getProducts = async () => {
+    try {
+      const data = await ProductApi.getProducts()
+      productDispatch({
+        type: "success",
+        results: data as ProductsType[],
+      })
+    } catch (err) {
+      console.log(typeof err)
+
+      if (axios.isAxiosError(err)) {
+        productDispatch({ type: "failure", error: err.message })
+        console.log(err)
+        const error = err
+        toast.error(
+          () => (
+            <div>
+              <p>{error.message}</p>
+              <button
+                onClick={() => refetchRequest(error)}
+                className="p-2 text-lg text-center bg-red-400 rounded-lg"
+              >
+                Try again
+              </button>
+            </div>
+          ),
+          {
+            pauseOnHover: true,
+          }
+        )
+      }
+    }
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
+  return {
+    products,
+    productsError,
+    productsLoading,
+  }
+}
+
+export default useProducts
