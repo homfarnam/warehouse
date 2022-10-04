84d08f0ccac4c92939c01982f7ff2fbe267ea7ed
homfarnam <homfarnam@gmail.com>
2022-10-05 01:01:46 +0200

    Use new components
    - Add loader component
    - Add product detail component

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,5 +1,4 @@
-import { ClipLoader } from "react-spinners"
-import { ArticleData, Layout } from "../components"
+import { Layout, Loader, ProductDetail } from "../components"
 import { useProducts } from "../components/hooks"
 
 const Products = () => {
@@ -8,35 +7,18 @@ const Products = () => {
   return (
     <Layout>
       <div className="products">
-        <section className="flex flex-col w-full h-full">
+        <section>
           <h1 className="products--title">Products</h1>
-          <div className="w-1/2 mx-auto my-10">
-            <p className="text-2xl font-medium text-center">
-              You can find products data here.
-            </p>
+          <div className="products__description">
+            <h3>You can find products data here.</h3>
           </div>
-          <div className="w-4/12 mx-auto my-10 space-y-3">
+          <div className="products__mainContent">
             {productsLoading ? (
-              <ClipLoader
-                className="flex items-center justify-center w-full my-10"
-                color="black"
-                loading={productsLoading}
-                size={20}
-              />
+              <Loader loading={productsLoading} size={20} />
             ) : (
               products.length > 0 &&
               products.map((product) => {
-                return (
-                  <div className="w-auto p-2 text-lg border" key={product.id}>
-                    <h3>{product.name}</h3>
-
-                    {product.articles &&
-                      product.articles.length > 0 &&
-                      product.articles.map((item) => (
-                        <ArticleData key={item.id} id={item.id} />
-                      ))}
-                  </div>
-                )
+                return <ProductDetail data={product} key={product.id} />
               })
             )}
           </div>
