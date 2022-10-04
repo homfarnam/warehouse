5a84540b74183060fdd7fe240be069d5af621774
homfarnam <homfarnam@gmail.com>
2022-10-05 00:16:31 +0200

    Add axios interceptor
    - Create config for retry request after get failed for 3 times

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,11 +1,39 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from "axios"
 
-const warehouseAPI = () =>
-  axios.create({
-    baseURL: 'http://localhost:8000',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+const warehouseAPI = axios.create({
+  baseURL: "http://localhost:8000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
 
-export { warehouseAPI };
+const CancelToken = axios.CancelToken
+
+type configType = AxiosRequestConfig & {
+  retry: number
+  retryDelay: number
+}
+
+const globalConfig: configType = {
+  retry: 4,
+  retryDelay: 1000,
+}
+
+warehouseAPI.interceptors.response.use(undefined, (err) => {
+  const { config } = err
+
+  if (!config || !config.retry) {
+    return Promise.reject(err)
+  }
+
+  config.retry -= 1
+  const delayRetryRequest = new Promise<void>((resolve) => {
+    setTimeout(() => {
+      console.log("retry the request", config.url)
+      resolve()
+    }, config.retryDelay || 1000)
+  })
+  return delayRetryRequest.then(() => warehouseAPI(config))
+})
+
+export { warehouseAPI, CancelToken, globalConfig }
