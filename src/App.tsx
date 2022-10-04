4722e86381c05b1ada21e08092247f256cc3c7b4
homfarnam <homfarnam@gmail.com>
2022-10-05 00:10:58 +0200

    Add sales route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 import Prodcuts from "./pages/products"
+import Sales from "./pages/sales"
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Prodcuts />} />
+      <Route path="/sales" element={<Sales />} />
     </Routes>
   )
 }
