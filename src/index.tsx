e56eef8904097c5bb328ec76484864dc85644000
homfarnam <homfarnam@gmail.com>
2022-10-07 00:09:58 +0200

    Wrap everything in context provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,23 @@
+import React from "react"
 import ReactDOM from "react-dom/client"
+import { ToastContainer } from "react-toastify"
+import { BrowserRouter } from "react-router-dom"
+import "react-toastify/dist/ReactToastify.css"
 import "./styles/tailwind.css"
 import "./styles/main.scss"
 import App from "./App"
-import React from "react"
-import { BrowserRouter } from "react-router-dom"
-import "react-toastify/dist/ReactToastify.css"
-import { ToastContainer } from "react-toastify"
+import WareHouseProvider from "./context/context"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-    <ToastContainer
-      newestOnTop
-      draggable
-      autoClose={2500}
-      hideProgressBar={false}
-    />
-  </BrowserRouter>
+  <WareHouseProvider>
+    <BrowserRouter>
+      <App />
+      <ToastContainer
+        newestOnTop
+        draggable
+        autoClose={2500}
+        hideProgressBar={false}
+      />
+    </BrowserRouter>
+  </WareHouseProvider>
 )
