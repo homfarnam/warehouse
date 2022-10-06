e96a5081faabaa5285e8de530b3cf5688ca8075d
homfarnam <homfarnam@gmail.com>
2022-10-07 00:11:10 +0200

    Import new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,19 @@
 import ArticleData from "./ArticleData/ArticleData"
 import ErrorDisplay from "./Shared/ErrorDisplay/ErrorDisplay"
 import Layout from "./Shared/Layout/Layout"
-import ProductData from "./ProductsData/ProductsData"
+import ProductData from "./ProductData/ProductData"
 import ProductDetail from "./ProductDetail/ProductDetail"
 import Loader from "./Shared/Loader/Loader"
+import SalesData from "./SalesData/SalesData"
+import ShopCart from "./ShopCart/ShopCart"
 
-export { ArticleData, ErrorDisplay, Layout, ProductData, ProductDetail, Loader }
+export {
+  ArticleData,
+  ErrorDisplay,
+  Layout,
+  ProductData,
+  ProductDetail,
+  Loader,
+  SalesData,
+  ShopCart,
+}
