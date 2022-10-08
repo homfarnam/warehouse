f60140fbae2823658ae504320ed2e2d815c9535e
homfarnam <homfarnam@gmail.com>
2022-10-09 00:39:02 +0200

    Add data-testId and react toast to show error

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,14 +1,31 @@
+import { toast } from "react-toastify"
 import { Layout, Loader, ProductDetail } from "../components"
 import { useProducts } from "../hooks"
 
 const Products = () => {
-  const { products, productsLoading, productRequest } = useProducts()
+  const { products, productsLoading, productsError } = useProducts()
+
+  if (productsError) {
+    toast.error(
+      () => (
+        <div data-testid="error-toast">
+          <p>{productsError}</p>
+        </div>
+      ),
+      {
+        pauseOnHover: true,
+        autoClose: 5000,
+      }
+    )
+  }
 
   return (
     <Layout>
       <div className="products">
         <section>
-          <h2 className="products--title">Products</h2>
+          <h2 className="products--title" data-testid="products-title">
+            Products
+          </h2>
           <div className="products__description">
             <h3>You can find products data here.</h3>
           </div>
@@ -17,9 +34,9 @@ const Products = () => {
               <Loader loading={productsLoading} size={20} />
             ) : (
               products.length > 0 &&
-              products.map((product) => {
-                return <ProductDetail data={product} key={product.id} />
-              })
+              products.map((product) => (
+                <ProductDetail data={product} key={product.id} />
+              ))
             )}
           </div>
         </section>
