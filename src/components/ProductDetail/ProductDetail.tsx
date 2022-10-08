f189f71d7668c2b5f4a13f316684efba61557cfc
homfarnam <homfarnam@gmail.com>
2022-10-08 14:12:29 +0200

    Add amount input and add to cart icon

--- src/components/ProductDetail/ProductDetail.tsx
+++ src/components/ProductDetail/ProductDetail.tsx
@@ -1,14 +1,46 @@
+import { useContext, useState } from "react"
 import type { ProductsType } from "../../types/api.types"
 import ArticleData from "../ArticleData/ArticleData"
+import { BsCartPlusFill } from "react-icons/bs"
+import { WarehouseContext } from "../../context/context"
 
 interface ProductDetailProps {
   data: ProductsType
 }
 
 const ProductDetail = ({ data }: ProductDetailProps) => {
+  const [amount, setAmount] = useState<number>(1)
+  const { saveToCart } = useContext(WarehouseContext)
+
+  const addToSale = async () => {
+    const cartData = { data, amount }
+    saveToCart(cartData)
+  }
+
+  const handleAmount = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(Number(e.target.value))
+  }
+
   return (
-    <div className="w-auto p-2 text-lg border">
-      <h3>{data.name}</h3>
+    <div className="productCard">
+      <div className="productCard__header">
+        <h3>{data.name}</h3>
+        <div className="productCard__header__cart">
+          <input
+            type="number"
+            value={amount}
+            className="productCard__header__cart--amountInput"
+            onChange={handleAmount}
+            min={1}
+            max={10}
+          />
+          <BsCartPlusFill
+            className="productCard__header__cart--icon"
+            size={27}
+            onClick={addToSale}
+          />
+        </div>
+      </div>
 
       {data.articles &&
         data.articles.length > 0 &&
